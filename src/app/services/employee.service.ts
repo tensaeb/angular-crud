824cd3507ce63ccd7e0bd29a0a614657a58e412f
Tensae Befekadu <39292585+tensaeb@users.noreply.github.com>
2023-10-16 17:05:19 +0300

    Update url

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -8,15 +8,23 @@ import { Observable } from 'rxjs';
 export class EmployeeService {
   constructor(private _http: HttpClient) {}
   addEmployee(data: any): Observable<any> {
-    return this._http.post('http://localhost:3000/employees', data);
+    return this._http.post(
+      'https://vercel-json-nine.vercel.app/employees',
+      data
+    );
   }
   getEmployeeList(): Observable<any> {
-    return this._http.get('http://localhost:3000/employees');
+    return this._http.get('https://vercel-json-nine.vercel.app/employees');
   }
   deleteEmployee(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:3000/employees/${id}`);
+    return this._http.delete(
+      `https://vercel-json-nine.vercel.app/employees/${id}`
+    );
   }
   updateEmployee(id: number, data: any): Observable<any> {
-    return this._http.put(`http://localhost:3000/employees/${id}`, data);
+    return this._http.put(
+      `https://vercel-json-nine.vercel.app/employees/${id}`,
+      data
+    );
   }
 }
